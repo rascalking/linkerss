b40acf0f768962e3c3dcb3b796c7b8baaf715395
Dave Bonner <david_bonner@harvard.edu>
2016-01-16 15:00:30 -0500

    item timestamp matches tweet, if rt use original author

--- linkerss.go
+++ linkerss.go
@@ -2,55 +2,51 @@ package main
 
 import (
 	"bytes"
-    "encoding/json"
 	"flag"
-    "fmt"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-    "regexp"
-    "strings"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/coreos/pkg/flagutil"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/gorilla/feeds"
-    "golang.org/x/net/html"
+	"golang.org/x/net/html"
 	"golang.org/x/oauth2"
 )
 
-
 var textHtml = regexp.MustCompile("^text/html($|;)")
-
 
 func main() {
 	// parse flags
 	flags := flag.NewFlagSet("linkerss", flag.ExitOnError)
 	accessToken := flags.String("app-access-token", "",
 		"Twitter application access token")
-    listenAddress := flags.String("listen-address", "0.0.0.0:9999",
-        "Address and port to listen on")
+	listenAddress := flags.String("listen-address", "0.0.0.0:9999",
+		"Address and port to listen on")
 	flags.Parse(os.Args[1:])
 	flagutil.SetFlagsFromEnv(flags, "TWITTER")
 	if *accessToken == "" {
 		log.Fatal("Application access token required")
 	}
 
-    // set up handlers
-    http.HandleFunc("/user", func(w http.ResponseWriter, req *http.Request) {
-        userHandler(accessToken, w, req)
-    })
+	// set up handlers
+	http.HandleFunc("/user", func(w http.ResponseWriter, req *http.Request) {
+		userHandler(accessToken, w, req)
+	})
 
-    // answer http requests
-    log.Println("Listening on " + *listenAddress)
-    log.Fatal(http.ListenAndServe(*listenAddress, nil))
+	// answer http requests
+	log.Println("Listening on " + *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
 }
 
-
 func userHandler(accessToken *string, w http.ResponseWriter, req *http.Request) {
-    log.Println("%v", req)
-    screenName := req.URL.Query().Get("screenName")
+	log.Println("%v", req)
+	screenName := req.URL.Query().Get("screenName")
 
 	// set up the twitter client
 	config := oauth2.Config{}
@@ -87,24 +83,30 @@ func userHandler(accessToken *string, w http.ResponseWriter, req *http.Request) 
 func tweetsToFeed(tweets []twitter.Tweet, screenName string) *feeds.Feed {
 	const htmlTemplate = `
     <div>
-    <a href="{{.Link.Href}}">{{.Title}}</a> via {{.Author.Name}}
+      <a href="{{.Link.Href}}">{{.Title}}</a>
+    </div>
+    <div>
+	via <a href="https://twitter.com/{{.Author.Name}}">{{.Author.Name}}</a>
     </div>
     `
 	templ := template.Must(template.New("item").Parse(htmlTemplate))
 
 	feed := &feeds.Feed{
-		Title:       fmt.Sprintf("@%s's linkerss", screenName),
-		Link:        &feeds.Link{Href: fmt.Sprintf("https://twitter.com/%s",
-                                                   screenName)},
-		Description: fmt.Sprintf("Tweets with links in @%s's timeline",
-                                 screenName),
-		Author:      &feeds.Author{screenName, ""},
-		Created:     time.Now(),
+		Title: fmt.Sprintf("@%s's linkerss", screenName),
+		Link: &feeds.Link{Href: fmt.Sprintf("https://twitter.com/%s",
+			screenName)},
+		Description: fmt.Sprintf("Links from tweets in @%s's timeline",
+			screenName),
+		Author:  &feeds.Author{screenName, ""},
+		Created: time.Now(),
 	}
 
+	// TODO: parallelize item generation, but retain original order
 	for _, t := range tweets {
-		// TODO - look at retweeted_status to find RTer, not original author
 		author := &feeds.Author{t.User.Name, ""}
+		if t.RetweetedStatus != nil {
+			author = &feeds.Author{t.RetweetedStatus.User.Name, ""}
+		}
 
 		for _, u := range t.Entities.Urls {
 			// fetch page
@@ -117,30 +119,33 @@ func tweetsToFeed(tweets []twitter.Tweet, screenName string) *feeds.Feed {
 
 			// figure out a title
 			title := u.ExpandedURL
-            switch {
-            case textHtml.MatchString(resp.Header.Get("Content-Type")):
-                doc, err := html.Parse(resp.Body)
-                if err != nil {
-                    log.Printf("unable to parse the body of %s: %s",
-                               u.ExpandedURL, err)
-                    continue
-                }
-                title = getTitle(doc)
-            // TODO: pdf, images?
-            }
+			switch {
+			case textHtml.MatchString(resp.Header.Get("Content-Type")):
+				doc, err := html.Parse(resp.Body)
+				if err != nil {
+					log.Printf("unable to parse the body of %s: %s",
+						u.ExpandedURL, err)
+					continue
+				}
+				title = getTitle(doc)
+				// TODO: pdf, images?
+			}
+
+			// parse out the timestamp
+			created, err := time.Parse(time.RubyDate, t.CreatedAt)
+			if err != nil {
+				log.Printf("unable to parse tweet's created_at value %s: %s",
+					t.CreatedAt, err)
+			}
 
 			// generate feed item
 			item := &feeds.Item{
-				Title:       title, // linked page title
-				Link:        &feeds.Link{Href: u.ExpandedURL},
+				Title: title, // linked page title
+				Link: &feeds.Link{Href: u.ExpandedURL},
 				Description: "", // "{LINK} via {USER} {TWEET}"
-				Author:      author,
-				//Created: t.CreatedAt, // TODO - parse string into time.Time
+				Author: author,
+				Created: created,
 			}
-            itemJson, _ := json.Marshal(item)
-            var buf bytes.Buffer
-            json.Indent(&buf, itemJson, "", "    ")
-            buf.WriteTo(os.Stdout)
 
 			// now build the item description
 			buffer := new(bytes.Buffer)
@@ -155,17 +160,16 @@ func tweetsToFeed(tweets []twitter.Tweet, screenName string) *feeds.Feed {
 	return feed
 }
 
-
 func getTitle(n *html.Node) string {
-    if n.Type == html.ElementNode && n.Data == "title" {
-        return strings.TrimSpace(n.FirstChild.Data)
-    }
+	if n.Type == html.ElementNode && n.Data == "title" {
+		return strings.TrimSpace(n.FirstChild.Data)
+	}
 
-    for c := n.FirstChild; c != nil; c = c.NextSibling {
-        title := getTitle(c)
-        if title != "" {
-            return title
-        }
-    }
-    return ""
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		title := getTitle(c)
+		if title != "" {
+			return title
+		}
+	}
+	return ""
 }
